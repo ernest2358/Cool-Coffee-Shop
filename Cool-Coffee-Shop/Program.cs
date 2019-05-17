6a68b0c3b29914750142d7135eeab6a2222f7a08
Hermann I McEnhill <hmcenhill@gmail.com>
2019-05-16 20:10:41 -0400

    Initial VS Files created

---
+++ Cool-Coffee-Shop/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Cool_Coffee_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Super Cool Coffee Shop Program!");
+            Console.ReadKey();
+        }
+    }
+}
